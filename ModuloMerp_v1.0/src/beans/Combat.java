1c4c7edcf16efd9be0adabf9fbd116fa6304712e
GABRIEL_RUEDA <GABRIEL_RUEDA@SAD-5786-12.endesa.es>
2016-06-23 17:21:46 +0200

    Combat.java

---
+++ ModuloMerp_v1.0/src/beans/Combat.java
@@ -0,0 +1,13 @@
+package beans;
+
+import java.util.Map;
+
+public class Combat {
+	
+	Map<String, Character> characters;
+
+	public Combat(Map<String, Character> characters) {
+		this.characters = characters;
+	}
+
+}
